809b2cab477bac25555c0c94e3d8bfb34bd2e60f
nchkss <nichickevich@gmail.com>
2023-01-18 16:06:20 +0200

    change json file toregular js file with object inside for compatibillity with ios devices per reason assert in import does not work in safari

--- js/tabs.js
+++ js/tabs.js
@@ -1,4 +1,4 @@
-import data from "../data.json" assert { type: "json" };
+import data from "./data.js";
 
 const tabList = document.querySelector('[role="tablist"]');
 const tabs = tabList.querySelectorAll('[role="tab"]');
